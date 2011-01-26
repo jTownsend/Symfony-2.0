53912c4cac52457304a17eb0059d042ec4f0dde5
jTownsend <jon.townsend@funsational.com>
2011-01-25 16:35:58 -0800

    Updated .gitignore, left navigation categories

--- src/Application/JonTestBundle/Controller/CategoryController.php
+++ src/Application/JonTestBundle/Controller/CategoryController.php
@@ -8,6 +8,37 @@ class CategoryController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JonTestBundle:Jon:category.twig.html');
+        $categories = array(
+			'name' 				=> 'Printable Games',
+			'categoriesSize' 	=> '4',
+			'children' 			=> array(
+				array(
+					'url' 		=> '',
+					'children' 	=> array(),
+					'name'		=> 'Sweet',
+					
+				),
+				array(
+					'url' 		=> '',
+					'children' 	=> array(),
+					'name'		=> 'Dude',
+					
+				),
+				array(
+					'url' 		=> '',
+					'children' 	=> array(),
+					'name'		=> 'Whoa',
+					
+				),
+				array(
+					'url' 		=> '',
+					'children' 	=> array(),
+					'name'		=> 'What',
+					
+				),
+			),		
+		);
+		
+		return $this->render('JonTestBundle:Jon:category.twig.html', array('categories' => $categories));
     }
 }
