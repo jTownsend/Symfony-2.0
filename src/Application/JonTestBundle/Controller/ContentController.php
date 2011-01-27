428a619c934d0de16bd25f4ee5b125060e08d1c1
jTownsend <jon.townsend@funsational.com>
2011-01-26 16:53:30 -0800

    Landing page mockup

--- src/Application/JonTestBundle/Controller/ContentController.php
+++ src/Application/JonTestBundle/Controller/ContentController.php
@@ -8,6 +8,11 @@ class ContentController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JonTestBundle:Jon:content.twig.html');
+        $content = array(
+			'banner' => 'images/assets/banners/billboards/baby_shower_games_atoz/billboard_final_baby.png',
+			'bingoSale' => 'images/assets/banners/sales/baby_shower_games_atoz/bingosale_babytitle.png'
+		);
+
+		return $this->render('JonTestBundle:Jon:content.twig.html', array('content' => $content));
     }
 }
